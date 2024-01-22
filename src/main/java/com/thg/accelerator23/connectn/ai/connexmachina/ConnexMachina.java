3a7efe2d8d42f80f6be27747fab54dcd00773cce
Zac Gladman <Zac.Gladman35@thehutgroup.com>
2024-01-22 11:18:13 +0000

    first commit

--- src/main/java/com/thg/accelerator23/connectn/ai/connexmachina/ConnexMachina.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/connexmachina/ConnexMachina.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.connexmachina;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ConnexMachina extends Player {
+  public ConnexMachina(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ConnexMachina.class.getName());
   }
 
   @Override
